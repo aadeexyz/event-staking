fa8fea1081319a8f25b7c00302c5008eef6911fa
Aadeetya <aadeetyapatil@gmail.com>
2023-04-06 04:08:02 -0400

    complete contract

--- src/EventStaking.sol
+++ src/EventStaking.sol
@@ -2,20 +2,28 @@
 
 pragma solidity 0.8.19;
 
-contract EventStaking {
+import "solmate/auth/Owned.sol";
+
+contract EventStaking is Owned {
     uint256 public eventStartTime;
     uint256 public eventEndTime;
     uint256 public totalAmoutRSVPd;
+    uint256 public claimableAmount;
     uint256 public claimedAmount;
+    bool public claimStarted;
 
     struct Guest {
         uint256 amount;
         bool present;
+        bool claimed;
     }
 
     mapping(address => Guest) public guestsMapping;
 
-    constructor(uint256 _eventStartTime, uint256 _eventEndTime) {
+    constructor(
+        uint256 _eventStartTime,
+        uint256 _eventEndTime
+    ) Owned(msg.sender) {
         eventStartTime = _eventStartTime;
         eventEndTime = _eventEndTime;
     }
@@ -26,6 +34,7 @@ contract EventStaking {
 
         guestsMapping[msg.sender] = Guest(
             guestsMapping[msg.sender].amount + msg.value,
+            false,
             false
         );
         totalAmoutRSVPd += msg.value;
@@ -48,15 +57,35 @@ contract EventStaking {
     }
 
     function claim() external {
-        require(block.timestamp >= eventEndTime, "Event has not ended yet");
+        require(claimStarted, "Claim has not started yet");
         require(guestsMapping[msg.sender].amount > 0, "You have not RSVP'd");
         require(
             guestsMapping[msg.sender].present,
             "You did not attend the event"
         );
+        require(!guestsMapping[msg.sender].claimed, "You have already claimed");
 
-        uint256 amount;
+        uint256 amount = (guestsMapping[msg.sender].amount / totalAmoutRSVPd) *
+            claimableAmount;
+
+        guestsMapping[msg.sender].claimed = true;
+        claimedAmount += guestsMapping[msg.sender].amount;
+        payable(msg.sender).transfer(amount);
     }
 
-    function withdrawStuckEther()
+    function startClaim() external onlyOwner {
+        require(block.timestamp >= eventEndTime, "Event has not ended yet");
+        require(!claimStarted, "Claim has already started");
+
+        claimStarted = true;
+        withdrawStuckEther();
+        claimableAmount = address(this).balance;
+    }
+
+    function withdrawStuckEther() private {
+        uint256 remainingAmount = totalAmoutRSVPd - claimedAmount;
+        uint256 amout = address(this).balance - remainingAmount;
+
+        payable(msg.sender).transfer(amout);
+    }
 }
