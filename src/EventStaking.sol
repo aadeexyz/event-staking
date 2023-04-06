cc1d442dc87904191e456e2a99973b8c7ca52640
Aadeetya <aadeetyapatil@gmail.com>
2023-04-06 05:00:56 -0400

    fixed calculations and added reentrancy guard

--- src/EventStaking.sol
+++ src/EventStaking.sol
@@ -3,8 +3,9 @@
 pragma solidity 0.8.19;
 
 import "solmate/auth/Owned.sol";
+import "solmate/utils/ReentrancyGuard.sol";
 
-contract EventStaking is Owned {
+contract EventStaking is Owned, ReentrancyGuard {
     uint256 public eventStartTime;
     uint256 public eventEndTime;
     uint256 public totalAmoutRSVPd;
@@ -26,6 +27,10 @@ contract EventStaking is Owned {
     ) Owned(msg.sender) {
         eventStartTime = _eventStartTime;
         eventEndTime = _eventEndTime;
+        totalAmoutRSVPd = 0;
+        claimableAmount = 0;
+        claimedAmount = 0;
+        claimStarted = false;
     }
 
     function stakeRSVP() external payable {
@@ -38,9 +43,10 @@ contract EventStaking is Owned {
             false
         );
         totalAmoutRSVPd += msg.value;
+        claimableAmount += msg.value;
     }
 
-    function attend() external {
+    function attend() external nonReentrant {
         require(block.timestamp >= eventStartTime, "Event has not started yet");
         require(block.timestamp < eventEndTime, "Event has already ended");
         require(guestsMapping[msg.sender].amount > 0, "You have not RSVP'd");
@@ -50,13 +56,13 @@ contract EventStaking is Owned {
         );
 
         guestsMapping[msg.sender].present = true;
-        payable(msg.sender).transfer(guestsMapping[msg.sender].amount);
-
-        totalAmoutRSVPd -= guestsMapping[msg.sender].amount;
         claimedAmount += guestsMapping[msg.sender].amount;
+        claimableAmount -= guestsMapping[msg.sender].amount;
+
+        payable(msg.sender).transfer(guestsMapping[msg.sender].amount);
     }
 
-    function claim() external {
+    function claim() external nonReentrant {
         require(claimStarted, "Claim has not started yet");
         require(guestsMapping[msg.sender].amount > 0, "You have not RSVP'd");
         require(
@@ -70,6 +76,8 @@ contract EventStaking is Owned {
 
         guestsMapping[msg.sender].claimed = true;
         claimedAmount += guestsMapping[msg.sender].amount;
+        claimableAmount -= guestsMapping[msg.sender].amount;
+
         payable(msg.sender).transfer(amount);
     }
 
@@ -77,15 +85,15 @@ contract EventStaking is Owned {
         require(block.timestamp >= eventEndTime, "Event has not ended yet");
         require(!claimStarted, "Claim has already started");
 
-        claimStarted = true;
         withdrawStuckEther();
-        claimableAmount = address(this).balance;
+        claimStarted = true;
     }
 
     function withdrawStuckEther() private {
-        uint256 remainingAmount = totalAmoutRSVPd - claimedAmount;
-        uint256 amout = address(this).balance - remainingAmount;
+        uint256 amount = address(this).balance - claimableAmount;
 
-        payable(msg.sender).transfer(amout);
+        if (amount > 0) {
+            payable(msg.sender).transfer(amount);
+        }
     }
 }
