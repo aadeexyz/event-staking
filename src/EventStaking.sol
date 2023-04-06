882541fcaf4d0521d60b086d7e917d33c47bc369
Aadeetya <aadeetyapatil@gmail.com>
2023-04-06 03:55:45 -0400

    added claim

--- src/EventStaking.sol
+++ src/EventStaking.sol
@@ -5,13 +5,15 @@ pragma solidity 0.8.19;
 contract EventStaking {
     uint256 public eventStartTime;
     uint256 public eventEndTime;
+    uint256 public totalAmoutRSVPd;
+    uint256 public claimedAmount;
 
     struct Guest {
         uint256 amount;
         bool present;
     }
 
-    mapping(address => Guest) public guests;
+    mapping(address => Guest) public guestsMapping;
 
     constructor(uint256 _eventStartTime, uint256 _eventEndTime) {
         eventStartTime = _eventStartTime;
@@ -22,16 +24,39 @@ contract EventStaking {
         require(block.timestamp <= eventEndTime, "Event has already ended");
         require(msg.value > 0, "You need to stake some ETH");
 
-        guests[msg.sender] = Guest(msg.value, false);
+        guestsMapping[msg.sender] = Guest(
+            guestsMapping[msg.sender].amount + msg.value,
+            false
+        );
+        totalAmoutRSVPd += msg.value;
     }
 
     function attend() external {
         require(block.timestamp >= eventStartTime, "Event has not started yet");
-        require(block.timestamp <= eventEndTime, "Event has already ended");
-        require(guests[msg.sender].amount > 0, "You have not RSVP'd");
-        require(!guests[msg.sender].present, "You have already attended");
+        require(block.timestamp < eventEndTime, "Event has already ended");
+        require(guestsMapping[msg.sender].amount > 0, "You have not RSVP'd");
+        require(
+            !guestsMapping[msg.sender].present,
+            "You have already attended"
+        );
 
-        guests[msg.sender].present = true;
-        payable(msg.sender).transfer(guests[msg.sender].amount);
+        guestsMapping[msg.sender].present = true;
+        payable(msg.sender).transfer(guestsMapping[msg.sender].amount);
+
+        totalAmoutRSVPd -= guestsMapping[msg.sender].amount;
+        claimedAmount += guestsMapping[msg.sender].amount;
     }
+
+    function claim() external {
+        require(block.timestamp >= eventEndTime, "Event has not ended yet");
+        require(guestsMapping[msg.sender].amount > 0, "You have not RSVP'd");
+        require(
+            guestsMapping[msg.sender].present,
+            "You did not attend the event"
+        );
+
+        uint256 amount;
+    }
+
+    function withdrawStuckEther()
 }
